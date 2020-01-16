a7d089bfdd3e770d88f53682ee30430ac0018435
ua-enumaelish <ua.enumaelish@gmail.com>
2020-01-16 17:52:02 +0200

    Upadate All

--- gallery - react/src/App.js
+++ gallery - react/src/App.js
@@ -7,14 +7,23 @@ export default class App extends React.Component{
       <div className="container">
         <div class="row bg-dark">
 			    <div class="col-2 pt-3">
-				    <a href="../index.html" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">BACK</a>	
+				    <a href="../../index.html" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">BACK</a>	
 			    </div>
 			  <div class="col-8 pt-2">
 				  <h1 class="mb-4 text-light text-center">
 					  Gallery in React
 				  </h1>
 			  </div>
-			<div class="offset-2"></div>
+        <div class="col-2 pt-3 pr-4 text-right">
+	      <a href="https://github.com/ua-enumaelish/Learning/tree/master/horse%20(lesson%2014)" 
+	        class="btn btn-secondary btn-lg active" 
+	        role="button" 
+	        aria-pressed="true"
+	        target="_blank"
+	      >
+	        GitHub
+	      </a>
+     </div>
 		</div>
         <div className="row pt-5">
         <div className="offset-2"></div>
